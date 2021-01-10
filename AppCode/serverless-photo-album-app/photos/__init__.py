06fab37cea9f1e223e25730d5293dd0f0408e61f
Bert Wagner <github@bertwagner.com>
2021-01-10 08:25:24 -0500

    init function app and function

---
+++ AppCode/serverless-photo-album-app/photos/__init__.py
@@ -0,0 +1,33 @@
+import os
+import logging
+import azure.functions as func
+from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import ContainerClient
+import json
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    
+    # Get HTTP parameters from URL, setting defaults if empty
+    folder = req.params.get('folder')
+
+    # Lookup the folder and return a list of blobs
+    container = ContainerClient.from_connection_string(conn_str=os.environ["STORAGE_CONNECTION_STRING"], container_name="photos")
+    blob_list = container.list_blobs(name_starts_with=folder)
+
+    # Create our response obejct
+    photo_response = {
+        "files": []
+    }
+
+    # Iterate the list to populate it
+    for blob in blob_list:
+        photo_response["files"].append(blob.name)
+        
+    
+    return func.HttpResponse(
+        json.dumps(photo_response),
+        status_code=200,
+        mimetype="application/json"
+    )
