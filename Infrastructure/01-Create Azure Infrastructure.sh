7132002596d77c1829354a292bc72d4bba72cec8
Bert Wagner <github@bertwagner.com>
2021-01-11 21:01:36 -0500

    adds static web hosting account

--- Infrastructure/01-Create Azure Infrastructure.sh
+++ Infrastructure/01-Create Azure Infrastructure.sh
@@ -27,6 +27,13 @@ az storage account create \
     --location $region \
     --sku Standard_LRS
 
+printf "\nUpdate storage account to allow static web hosting"
+az storage blob service-properties update \
+    --account-name $storageAccountName  \
+    --static-website \
+    --404-document 404.html \
+    --index-document index.html
+
 printf "\nCreate a container to store the blobs in"
 # We make the public access level "blob" so files can be read anonymously in the browser
 az storage container create \
@@ -46,6 +53,11 @@ az functionapp create \
     --runtime python \
     --runtime-version 3.8 
 
-printf "\nDeploying the function app code..."
-# cd AppCode/FunctionAlbumPhotos/AlbumPhotos
-# func azure functionapp publish album-photos
+printf "\nUpdate the function app to allow CORS origins from our storage account..."
+az functionapp cors add --name $functionAppName --resource-group $resourceGroupName --allowed-origins "https://$storageAccountName.z13.web.core.windows.net"
+az resource update --name web --resource-group $resourceGroupName --namespace Microsoft.Web --resource-type config --parent sites/$functionAppName --set properties.cors.supportCredentials=true
+
+#printf "\nSetup Azure CDN to allow custom domain over HTTPS..."
+#https://docs.microsoft.com/en-us/azure/storage/blobs/storage-custom-domain-name?tabs=azure-portal#map-a-custom-domain-with-https-enabled
+
+
