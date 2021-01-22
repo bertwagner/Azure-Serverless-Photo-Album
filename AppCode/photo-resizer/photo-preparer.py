6b488863384a76fd646c391d7582e3881d370979
Bert Wagner <bertwagner@bertwagner.com>
2021-01-21 20:42:42 -0500

    add json meta file output

--- AppCode/photo-resizer/photo-preparer.py
+++ AppCode/photo-resizer/photo-preparer.py
@@ -2,6 +2,9 @@ import argparse
 import os
 import pathlib
 import cv2
+from dotenv import load_dotenv, find_dotenv
+from azure.storage.blob import BlobClient
+import json
 
 def create_thumbs_folder(folder_path):
     thumbs_path = pathlib.Path(os.path.join(folder_path,"thumbs"))
@@ -11,27 +14,15 @@ def create_thumbs_folder(folder_path):
 
     return thumbs_path
 
-def rename_filename_with_dimensions(folder,file_root,file_extension):
-    filename = os.path.join(folder,file_root+file_extension)
+def get_image_dimensions(folder,filename):
+    filename = os.path.join(folder,filename)
 
     # Get photo dimensions
     photo = cv2.imread(filename)
     width = photo.shape[1]
     height = photo.shape[0]
-    dimensions = f"{width}x{height}"
-
-    # Append photo dimensions if they don't exist 
-    file_root_with_dimensions = f"{file_root}"
-    filename_with_dimensions = file_root+file_extension
-    if not file_root.endswith(dimensions):
-        file_root_with_dimensions = f"{file_root} {dimensions}"
-        filename_with_dimensions = f"{file_root_with_dimensions}{file_extension}"
-        
-        # Rename file
-        os.rename(filename,os.path.join(folder,filename_with_dimensions))
     
-
-    return filename_with_dimensions,file_root_with_dimensions
+    return width, height
 
 def calculate_thumnail_dimensions(width,height,final_max_dimension):
     if width > height:
@@ -43,12 +34,10 @@ def calculate_thumnail_dimensions(width,height,final_max_dimension):
     
     return (new_width,new_height)
 
-def create_thumbnail(folder, thumbs_folder, file_root, file_root_with_dimensions, file_extension):
-
-    filename = os.path.join(folder,file_root+file_extension)
+def create_thumbnail(folder, thumbs_folder, filename):
 
     # Get photo dimensions
-    photo = cv2.imread(os.path.join(folder,file_root_with_dimensions+file_extension))
+    photo = cv2.imread(os.path.join(folder,filename))
     width = photo.shape[1]
     height = photo.shape[0]
 
@@ -57,28 +46,39 @@ def create_thumbnail(folder, thumbs_folder, file_root, file_root_with_dimensions
     # Resize image
     resized_image = cv2.resize(photo,thumb_dimensions,interpolation=cv2.INTER_CUBIC)
 
-    # file_root may already have dimensions appended if previously processed - strip the dimensions
-    file_parts = file_root.split()
-    file_root = " ".join(file_parts[:-1]) if len(file_parts) > 1 else file_parts[0]
-
-    # Name the file
-    dimensions = f"{thumb_dimensions[0]}x{thumb_dimensions[1]}"
-    thumbnail_file = f"{file_root} {dimensions}{file_extension}"
-    thumbnail_filename = os.path.join(thumbs_folder,thumbnail_file)
-
     # Save thumbnail
-    cv2.imwrite(thumbnail_filename,resized_image)
+    cv2.imwrite(os.path.join(thumbs_folder,filename),resized_image)
+    
+    return resized_image, thumb_dimensions[0], thumb_dimensions[1]
+
+def upload_to_blob_storage(local_folder, filename, blob_folder_name):
+   
+    blob = BlobClient.from_connection_string(conn_str=os.environ["AzureWebJobsStorage"], container_name="photos", blob_name=os.path.join(blob_folder_name,filename))
+
+    with open(os.path.join(local_folder,filename), "rb") as data:
+        blob.upload_blob(data)
 
 
 if __name__ == "__main__":
     # Parse the folder path from command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("folder")
+    parser.add_argument("name")
     args = parser.parse_args()
     folder = args.folder
+    name = args.name
+    
+    # Load entries from .env for Azure blob storage credentials
+    # The .env file should contain a value like:
+    # AzureWebJobsStorage="DefaultEndpointsProtocol=https;AccountName=...<rest of connection string>..."
+    dotenv_path = find_dotenv() 
+    load_dotenv(dotenv_path)
 
     # Create a thumbs folder if it doesn't exist
     thumbs_folder = create_thumbs_folder(folder)
+
+    # Declare an object for storing all of our photo meta data
+    file_paths = []
 
     # Iterate over each file
     photo_extensions = [".gif",".png",".tif",".jpg",".jpeg"]
@@ -86,11 +86,25 @@ if __name__ == "__main__":
         filename = os.fsdecode(file)
 
         if any(extension in filename for extension in photo_extensions):
-        
-            file_root, file_extension = os.path.splitext(filename)
+            file_info = {}
+            file_info["name"] = filename
 
-            filename_with_dimensions, file_root_with_dimensions = rename_filename_with_dimensions(folder,file_root,file_extension)
+            width, height = get_image_dimensions(folder,filename)
+            file_info["width"] = width
+            file_info["height"] = height
 
-            create_thumbnail(folder, thumbs_folder, file_root, file_root_with_dimensions, file_extension)
+            # Create a thumbnail, returning the thumbnail and dimensions
+            thumbnail_filename, thumbnail_width, thumbnail_height = create_thumbnail(folder, thumbs_folder, filename)
+            file_info["thumbnail_width"] = thumbnail_width
+            file_info["thumbnail_height"] = thumbnail_height
 
-            # Upload all files to Azure
+            #upload_to_blob_storage(folder, filename, name)
+            #upload_to_blob_storage(folder+"/thumbs", filename, name+"/thumbs")
+
+            file_paths.append(file_info)
+
+    # Save the image meta data to a json file and upload to blob storage
+    json_filename = os.path.join(folder,"photos.json")
+    with open(json_filename,"w") as f:
+        json.dump(file_paths,f)
+    upload_to_blob_storage(folder, "photos.json", name)
